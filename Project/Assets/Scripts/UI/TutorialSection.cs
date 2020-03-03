b3a744d5121b79ff690d7767898d6198ece8df7b
Michael Ensly <mensly@twobulls.com>
2020-03-03 12:02:16 +1100

    A tutorial text

---
+++ Project/Assets/Scripts/UI/TutorialSection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Platformer.Mechanics;
+
+[RequireComponent(typeof(Collider2D))]
+public class TutorialSection : MonoBehaviour
+{
+    public Canvas canvas;
+    public Text tutorial;
+    [TextArea(3, 5)]
+    public string text;
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider);
+        var p = collider.gameObject.GetComponent<PlayerController>();
+        if (p != null)
+        {
+            if (text.Length == 0)
+            {
+                canvas.gameObject.SetActive(false);
+            }
+            else
+            {
+                canvas.gameObject.SetActive(true);
+                tutorial.text = text;
+            }
+        }
+    }
+}
